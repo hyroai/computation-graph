64404c75af0537549dd6be92ee5007aab19fe4a7
uri <uri.valevski@gmail.com>
2021-04-26 21:16:55 +0300

    simplify debugger code

--- computation_graph/debug.py
+++ computation_graph/debug.py
@@ -1,29 +1,36 @@
-from typing import Callable, Text, Tuple
+from typing import Iterable, Tuple
 
 import gamla
 
 from computation_graph import base_types, graph, run, visualization
 
-_ComputationTrace = Callable[
-    [base_types.ComputationNode],
-    Tuple[Tuple[base_types.ComputationNode, base_types.ComputationResult]],
-]
 
-
-def _node_computation_trace(node_to_results: run.NodeToResults) -> _ComputationTrace:
-    return gamla.compose_many_to_one(
+def _node_computation_trace(
+    node_to_results: run.NodeToResults, node: base_types.ComputationNode
+) -> Iterable[Tuple[base_types.ComputationNode, base_types.ComputationResult]]:
+    return frozenset(
         [
-            gamla.pair_right(gamla.compose_left(node_to_results, gamla.head)),
-            gamla.compose_left(node_to_results, dict.values, gamla.head, dict.items),
-        ],
-        gamla.prefix,
+            (node, gamla.head(node_to_results(node))),
+            *gamla.pipe(node, node_to_results, dict.values, gamla.head, dict.items),
+        ]
     )
 
 
 @gamla.curry
-def debugger(
-    filename: Text, edges: base_types.GraphType, node_to_results: run.NodeToResults
+def serialize_computation_trace(
+    filename: str,
+    graph_instance: base_types.GraphType,
+    node_to_results: run.NodeToResults,
 ):
-    return visualization.serialize_computation_trace(filename)(
-        [edges, _node_computation_trace(node_to_results)(graph.infer_graph_sink(edges))]
+    gviz = visualization.union_graphviz(
+        [
+            visualization.computation_graph_to_graphviz(graph_instance),
+            visualization.computation_trace_to_graphviz(
+                _node_computation_trace(
+                    node_to_results, graph.infer_graph_sink(graph_instance)
+                )
+            ),
+        ]
     )
+    gviz.layout(prog="dot")
+    gviz.write(filename)
--- computation_graph/visualization.py
+++ computation_graph/visualization.py
@@ -1,4 +1,4 @@
-from typing import Callable, Text, Tuple
+from typing import Callable, Iterable, Text, Tuple
 from xml.sax import saxutils
 
 import gamla
@@ -45,7 +45,7 @@ def _handle_edge(pgv_graph, edge):
             )
 
 
-def _computation_graph_to_graphviz(edges: base_types.GraphType) -> pgv.AGraph:
+def computation_graph_to_graphviz(edges: base_types.GraphType) -> pgv.AGraph:
     pgv_graph = pgv.AGraph(directed=True)
     for edge in edges:
         _handle_edge(pgv_graph, edge)
@@ -64,53 +64,47 @@ def _do_add_node(result_graph: pgv.AGraph) -> Callable[[pgv.Node], pgv.AGraph]:
     )
 
 
-def _union_graphviz(graphs: Tuple[pgv.AGraph, ...]) -> pgv.AGraph:
-    return gamla.pipe(
-        graphs,
-        # copy to avoid side effects that influence caller
-        gamla.juxt(gamla.compose_left(gamla.head, pgv.AGraph.copy), gamla.drop(1)),
-        gamla.star(
-            gamla.reduce(
-                lambda result_graph, another_graph: gamla.pipe(
-                    another_graph,
-                    # following does side effects on result_graph, that's why we return just(result_graph)
-                    # we assume no parallelization in gamla.juxt
-                    gamla.side_effect(
-                        gamla.juxt(
-                            gamla.compose_left(
-                                pgv.AGraph.nodes,
-                                gamla.map(_do_add_node(result_graph)),
-                                tuple,
-                            ),
-                            gamla.compose_left(
-                                pgv.AGraph.edges,
-                                gamla.map(_do_add_edge(result_graph)),
-                                tuple,
-                            ),
-                        )
-                    ),
-                    gamla.just(result_graph),
-                )
+union_graphviz: Callable[[Iterable[pgv.AGraph]], pgv.AGraph] = gamla.compose_left(
+    # copy to avoid side effects that influence caller
+    gamla.juxt(gamla.compose_left(gamla.head, pgv.AGraph.copy), gamla.drop(1)),
+    gamla.star(
+        gamla.reduce(
+            lambda result_graph, another_graph: gamla.pipe(
+                another_graph,
+                # following does side effects on result_graph, that's why we return just(result_graph)
+                # we assume no parallelization in gamla.juxt
+                gamla.side_effect(
+                    gamla.juxt(
+                        gamla.compose_left(
+                            pgv.AGraph.nodes,
+                            gamla.map(_do_add_node(result_graph)),
+                            tuple,
+                        ),
+                        gamla.compose_left(
+                            pgv.AGraph.edges,
+                            gamla.map(_do_add_edge(result_graph)),
+                            tuple,
+                        ),
+                    )
+                ),
+                gamla.just(result_graph),
             )
-        ),
-    )
+        )
+    ),
+)
 
 
 def _save_graphviz_as_png(filename: Text) -> Callable[[pgv.AGraph], pgv.AGraph]:
     return gamla.side_effect(lambda pgv_graph: pgv_graph.draw(filename, prog="dot"))
 
 
-def _save_graphviz_as_dot(filename: Text) -> Callable[[pgv.AGraph], pgv.AGraph]:
-    return gamla.side_effect(lambda pgv_graph: pgv_graph.write(filename))
-
-
-def _computation_trace_to_graphviz(
-    computation_trace: Tuple[
+def computation_trace_to_graphviz(
+    computation_trace: Iterable[
         Tuple[base_types.ComputationNode, base_types.ComputationResult]
     ]
 ) -> pgv.AGraph:
     pgv_graph = pgv.AGraph()
-    for (node, result) in computation_trace:
+    for node, result in computation_trace:
         _add_computation_node(pgv_graph, node)
         graphviz_node = pgv_graph.get_node(hash(node))
         graphviz_node.attr["color"] = "red"
@@ -121,17 +115,5 @@ def _computation_trace_to_graphviz(
 
 
 visualize_graph = gamla.compose_left(
-    _computation_graph_to_graphviz, _save_graphviz_as_png("topology.png")
-)
-serialize_computation_trace = gamla.compose_left(
-    _save_graphviz_as_dot,
-    gamla.before(
-        gamla.compose_left(
-            gamla.stack(
-                [_computation_graph_to_graphviz, _computation_trace_to_graphviz]
-            ),
-            _union_graphviz,
-            gamla.side_effect(lambda g: g.layout(prog="dot")),
-        )
-    ),
+    computation_graph_to_graphviz, _save_graphviz_as_png("topology.png")
 )
