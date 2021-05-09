efad9e9587b8c24aa0f6e98a8a7bf413b5e43cfd
itayzit <itay@hyro.ai>
2021-05-09 18:43:56 +0300

    change call

--- computation_graph/run.py
+++ computation_graph/run.py
@@ -481,7 +481,7 @@ def to_callable_with_side_effect(
 
 # Use the second line if you want to see the winning path in the computation graph (a little slower).
 to_callable = gamla.curry(to_callable_with_side_effect)(gamla.just(gamla.just(None)))
-# to_callable = gamla.curry(to_callable_with_side_effect)(debug.debugger('utterance_computation.dot'))
+# to_callable = gamla.curry(to_callable_with_side_effect)(debug.serialize_computation_trace('utterance_computation.dot'))
 
 
 def _log_handled_exception(exception_type: Type[Exception]):
