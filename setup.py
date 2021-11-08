cc1ca4e025ab7859cf0ec34bc007e88681c16d73
itayzit <itay@hyro.ai>
2021-11-08 11:06:59 +0200

    bump version to 23 - future edges

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="computation-graph",
-    version="22",
+    version="23",
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
