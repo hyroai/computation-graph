eaf2a8b1be2f4c71c4e279f7968323ecf8ce398d
Uri <uri.valevski@gmail.com>
2021-12-23 15:55:43 +0200

    better-test

--- computation_graph/composers/composers_test.py
+++ computation_graph/composers/composers_test.py
@@ -1,18 +1,20 @@
-import time
-
 from computation_graph import composers, graph, run
 
 
 def test_ambiguity_does_not_blow_up():
-    start_time = time.time()
+    counter = 0
+
+    def increment():
+        nonlocal counter
+        counter += 1
 
     def g(x):
         return x
 
     for _ in range(15):
         g = composers.make_first(
-            composers.compose_left_unary(g, lambda x: x + "a"),
-            composers.compose_left_unary(g, lambda x: x + "b"),
+            composers.compose_left_unary(g, lambda x: increment() or x + "a"),
+            composers.compose_left_unary(g, lambda x: increment() or x + "b"),
         )
     run.to_callable(graph.connect_default_terminal(g), frozenset())(x="x")
-    assert time.time() - start_time < 1
+    assert counter == 30
