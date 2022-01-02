e0820f3811ab9c0c565ea8f7dd5fe1710222ab7b
Nitzan Bar <nitzanbar@gmail.com>
2022-01-02 16:30:01 +0200

    change gamla requirement

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setuptools.setup(
     long_description=_LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=setuptools.find_namespace_packages(),
-    install_requires=["gamla>=103", "typeguard", "toposort", "immutables"],
+    install_requires=["gamla>=117", "typeguard", "toposort", "immutables"],
     extras_require={"test": ["pygraphviz", "pytest>=5.4.0"]},
 )
