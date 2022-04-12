41f6d4685db16d1594dd6fdb9499d018225e39b3
eli <eli@hyro.ai>
2022-04-12 14:06:49 +0300

    allow compose_unary to a graph destination

--- computation_graph/composers/composers_test.py
+++ computation_graph/composers/composers_test.py
@@ -1,6 +1,16 @@
 from computation_graph import composers, graph_runners
 
 
+def test_unary_composition_with_graph_destination():
+    result = graph_runners.nullary_infer_sink(
+        composers.compose_unary(
+            composers.compose_left(lambda: 1, lambda a, b: a - b, key="a"), lambda: 2
+        )
+    )
+
+    assert result == -1
+
+
 def test_ambiguity_does_not_blow_up():
     counter = 0
 
